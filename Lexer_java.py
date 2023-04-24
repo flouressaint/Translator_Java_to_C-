8a10d837091240b52b37edc6b23803978b488b7f
ccf2p <yura.smolyaninov.01@mail.ru>
2023-04-24 19:25:24 +1000

    Token.py НЕ НУЖОН

--- Lexer_java.py
+++ Lexer_java.py
@@ -1,113 +1,198 @@
+import Token
+import re
+
+ACCESS_MODIFIERS = {
+    "public": "PUBLIC",
+    "private": "PRIVATE",
+    "protected": "PROTECTED",
+}
+
+KEY_WORDS = {
+    "class": "CLASS",
+    "else": "ELSE",
+    "for": "FOR",
+    "if": "IF",
+    "new": "NEW",
+    "package": "PACKAGE",
+    "return": "RETURN",
+    "static": "STATIC",
+    "this": "THIS",
+    "while": "WHILE",
+}
+
+DATA_TYPES = {
+    "byte": "BYTE",
+    "short": "SHORT",
+    "int": "INT",
+    "long": "LONG",
+    "float": "FLOAT",
+    "double": "DOUBLE",
+    "boolean": "boolean",
+    "char": "CHAR",
+    "string": "STRING",
+    "void": "VOID",
+}
+
+OPERATORS = {
+    "=": "ASSIGN",
+    "==": "IS_EQUAL",
+    "!=": "INEQUALITY",
+    "<": "LESS",
+    ">": "MORE",
+    ">=": "GRATER_THAN_OR_EQUAL",
+    "<=": "LESS_THAN_OR_EQUAL",
+    "+": "PLUS",
+    "-": "MINUS",
+    "*": "MULTIPLE",
+    "/": "DIVIDE",
+    "%": "MOD",
+    "++": "INCREMENT",
+    "--": "DECREMENT",
+    "+=": "EDITION",
+    "-=": "SUBTRACTION",
+    "*=": "MULTIPLICATION",
+    "/=": "DIVISION",
+    "%=": "MODULUS",
+    "&&": "AND",
+    "||": "OR",
+    "!": "NOT",
+    "(": "LEFT_BRACKET",
+    ")": "RIGHT_BRACKET",
+    "[": "LEFT_SQUARE_BRACKET",
+    "]": "RIGHT_SQUARE_BRACKET",
+    "{": "LEFT_CURL_BRACKET",
+    "}": "RIGHT_CURL_BRACKET",
+    ".": "DOT",
+    ";": "SEMICOLON",
+    ",": "SEMI"
+}
+
+
+IGNORE = ["\n", " ", "\t", ":"]
+
+
 class Token:
     def __init__(self, name, value):
         self.name = name
         self.value = value
 
     def __repr__(self):
-        return f"{self.name}\t{self.value}"
+        return f"{self.name}\t:{self.value}"
+
 
 class Lexer:
     def __init__(self, source):
+        self.state = None
         self.source = source
-        self.pos = 0
-        self.token = Token(None, None)
+        self.pos = -1
+        self.len = -1
 
-    def _next_token(self):
-        if self.pos >= len(self.source):
-            return Token('eof', None)
-        ch = self.source[self.pos]
+    """
+    Функция get_char смещает наш указатель (self.pos), потом
+    возвращает символ из текста программы и, если наш указатель
+    превысил длину строки то значит мы дошли до конца файла
+    и ставим наш статус равный "EOF" (end of file)
+    """
+    def get_char(self, text):
         self.pos += 1
-        if ch.isnumeric(): return Token('num', int(ch))
-        if ch.isalpha(): return Token('id', ch)
-        return Token(ch, None)
+        if self.pos < self.len:
+            return text[self.pos]
+        else:
+            self.state = "EOF"
+            return ""
 
-    def next_token(self):
-        self.token = self._next_token()
-        return self.token
-    
-import re
+    def get_lexem(self, text):
+        accum = ""  # накапливает символы из текста программы
+        self.state = "START"  # начальный статус
+        ch = self.get_char(text)
 
-# Define regular expressions for each token
-regex_patterns = {
-    'VAR': r'var',
-    'PROGRAM': r'program',
-    'BEGIN': r'begin',
-    'END': r'end',
-    'INTEGER': r'integer',
-    'REAL': r'real',
-    'BOOLEAN': r'boolean',
-    'CHAR': r'char',
-    'ARRAY': r'array',
-    'OF': r'of',
-    'IF': r'if',
-    'THEN': r'then',
-    'ELSE': r'else',
-    'WHILE': r'while',
-    'DO': r'do',
-    'REPEAT': r'repeat',
-    'UNTIL': r'until',
-    'FOR': r'for',
-    'TO': r'to',
-    'DOWNTO': r'downto',
-    'FUNCTION': r'function',
-    'PROCEDURE': r'procedure',
-    'NOT': r'not',
-    'IDENTIFIER': r'[a-zA-Z][a-zA-Z0-9_]*',
-    'NUMBER': r'[0-9]+',
-    'PLUS': r'\+',
-    'MINUS': r'\-',
-    'MULTIPLY': r'\*',
-    'DIVIDE': r'\/',
-    'ASSIGN': r':=',
-    'SEMICOLON': r';',
-    'LPAREN': r'\(',
-    'RPAREN': r'\)',
-    'DOT': r'\.',
-    'COMMA': r',',
-    'COLON': r':',
+        """
+        Разбираем текст программы до тех пор, пока наш статус не
+        None или не конец файла "EOF"
+        """
+        while self.state is not None and self.state != "EOF":
+            while ch in IGNORE:
+                ch = self.get_char(text)
 
-}
+            """
+            Если первым символом встретили не букву и не цифру, то проверяем находится ли
+            символ (ch) в OPERATORS, если да, то накапливаем все не буквы и не цифры,
+            которые идут следом и как только встретим что-то из OPERATORS, то возвращаем
+            полученный результат
+            
+            ============================ Возможны ошибки ============================
+            Надо проверить, что будет если наша последовательность не попадет в OPERATORS
+            """
+            if not ch.isalpha() and not ch.isnumeric():
+                while not ch.isalnum():
+                    accum += ch
+                    if accum in OPERATORS:
+                        return Token(accum, OPERATORS[accum])
+                    ch = self.get_char(text)
 
-# Combine all the regex patterns into one regular expression
-token_regex = re.compile('|'.join('(?P<%s>%s)' % pair for pair in regex_patterns.items()))
+            """
+            Если первым символом встретили букву, то переходим в состояние "ID" и начинаем собирать
+            следующие буквы и/или цифры до тех пор, пока собранная последовательность
+            не попадет в ACCESS_MODIFIERS, KEY_WORDS или DATA_TYPES. Если же не одну из
+            категорий не попало, то значит это ID
+            
+            ============================ Возможны ошибки ============================
+            Если в других методах не можете найти ошибку, то быть может она тут
+            """
+            if ch.isalpha():
+                while ch.isalnum():
+                    self.state = "ID"
+                    accum += ch
+                    if accum in ACCESS_MODIFIERS:
+                        return Token(accum, ACCESS_MODIFIERS[accum])
+                    elif accum in KEY_WORDS:
+                        return Token(accum, KEY_WORDS[accum])
+                    elif accum in DATA_TYPES:
+                        return Token(accum, DATA_TYPES[accum])
+                    ch = self.get_char(text)
+                self.pos -= 1
+                return Token(accum, "ID")
+
+            """
+            Если первым символом встретили цифру, то переходим в состояние "NUM"
+            и начинаем собирать следующие цифры.
+            Если встретили точку, то продолжаем собирать число.
+            Все выше сказанное делаем до тех пор, пока не встретим не цифру или не точку
+            
+            ============================ Возможны ошибки ============================
+            Надо проверять
+            """
+            if ch.isnumeric():
+                self.state = "NUM"
+                while ch.isnumeric():
+                    accum += ch
+                    ch = self.get_char(text)
+                    if ch == ".":
+                        accum += ch
+                        ch = self.get_char(text)
+                # Если наше число заканчивается на точку, то выкидываем ошибку
+                if accum[len(accum) - 1] == ".":
+                    raise SyntaxError("Invalid real number, in position {}".format(self.pos))
+
+                self.pos -= 1
+                return Token(accum, "NUM")
+
+    """
+    Начало начало - метод parse. Он открывает файл с исходным кодом на Java
+    считывает оттуда все строки и пошло поехало...
+    """
+    def parse(self):
+        with open(self.source, "r") as file:
+            text = file.read()
+
+        if len(text) == 0:
+            return
+        self.len = len(text)
+        while self.state != "EOF":
+            c = self.get_lexem(text)
+            if c is not None:
+                print(c)  # Здесь выводятся все наши токены потом можно как-то собрать
 
 
-def tokenize(code):
-    tokens = []
-    line_number = 1
-    for match in token_regex.finditer(code):
-        token_type = match.lastgroup
-        token_value = match.group(token_type)
-        if token_type == 'IDENTIFIER':
-            if not re.match(r'^[a-zA-Z]+$', token_value):
-                raise ValueError(f'SyntaxError: Invalid variable name "{token_value}" on line {line_number}')
-        elif token_type == 'NUMBER':
-            try:
-                int_value = int(token_value)
-            except ValueError:
-                raise ValueError(f'SyntaxError: Invalid integer "{token_value}" on line {line_number}')
-            if not -32768 <= int_value <= 32767:
-                raise ValueError(f'ValueError: Integer overflow "{token_value}" on line {line_number}')
-        elif token_type == 'SEMICOLON':
-            line_number += 1
-        tokens.append((token_type, token_value))
-    return tokens
-
-
-def main():
-    with open('file.pas', 'r') as file:
-        code = file.read()
-
-    try:
-        tokens = tokenize(code)
-        for token in tokens:
-            print(token)
-    except ValueError as e:
-        print('Error:', str(e))
-        return
-
-    print('Parsed successfully!')
-
-
-if __name__ == '__main__':
-    main()
+class SyntaxError(BaseException):
+    pass
---
+++ main.py
@@ -0,0 +1,10 @@
+from Lexer_java import *
+
+
+def main():
+    l = Lexer("C:\Translator_Java_to_C-\input.txt")
+    l.parse()
+
+
+if __name__ == '__main__':
+    main()
